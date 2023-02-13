0f7ac13092b73c9ac3995e56cb1645ac4e2eb591
byc <byckg3@outlook.com>
2023-02-13 20:19:56 +0800

    add main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region                   = "ap-northeast-1"
+  shared_credentials_files = ["~/.aws/credentials"]
+}
+
+resource "aws_instance" "hellotf" {
+  ami           = "ami-06ee4e2261a4dc5c3"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "tf"
+  }
+
+}
